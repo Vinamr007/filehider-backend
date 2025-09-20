f8f7c02ddfcf61da9b274208df1ccb4fff9c06fe
VINAMR NAIK <vinamrnaik206@gmail.com>
2025-09-20 09:28:15 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,70 @@
+package com.example.hosteltrack;
+
+public class User {
+    private String username;
+    private String email;
+    private String phone;
+    private String role;
+    private String enrnumber; // New field for the user's enrollment number
+    private String time;
+
+    public User() {
+        // Default constructor required for Firestore
+    }
+
+
+
+    public User(String username, String email,String enrnumber, String phone, String role,String time) {
+        this.username = username;
+        this.email = email;
+        this.phone = phone;
+        this.role = role;
+        this.enrnumber = enrnumber;
+        this.time = time;
+    }
+
+    // Getter and setter for username
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Getter and setter for email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Getter and setter for phone
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    // Getter and setter for role
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    // Getter and setter for enrollment number
+    public String getEnrnumber() {
+        return enrnumber;
+    }
+
+    public void setEnrnumber(String enrnumber) {
+        this.enrnumber = enrnumber;
+    }
+}
